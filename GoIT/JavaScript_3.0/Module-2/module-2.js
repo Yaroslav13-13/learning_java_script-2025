7d3be1108614e96da8fe75faf7cbae2f0ecbef48
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-11-13 22:51:46 +0200

    Learning-JS

--- GoIT/JavaScript_3.0/Module-2/module-2.js
+++ GoIT/JavaScript_3.0/Module-2/module-2.js
@@ -30,23 +30,102 @@ console.log("| Тренінг |");
 const word = "JavaScript";
 console.log(word.slice(0, 4)); // "Java"
 
+// ***********************************************
+
 const hello = "Hello, world";
 console.log(hello.slice(length - 3)); // "rld"
+
+// ***********************************************
 
 const str = "SaMsUnG";
 console.log(str.toLowerCase()); // "samsung"
 
+// ***********************************************
+
 const text = "BUY NOW! This is your chance!";
 console.log(text.toLowerCase().includes("buy"));
+
+// ***********************************************
 
 const file = "photo.jpeg";
 console.log(file.includes(".jpeg"));
 
+// ***********************************************
+
 const helloWorld = " Hello JavaScript ";
 console.log(helloWorld.trim());
+
+// ***********************************************
 
 const frontend = "Front end developer";
 console.log(frontend.slice(0, 5)); // "FRONT"
 
+// ***********************************************
+
 const name = "Jacob Mercer";
 console.log(name.includes("Mercer")); // true
+
+// ***********************************************
+
+function registerUser(mode) {
+  const message = "Hello world";
+  if (mode === "upper") {
+    return message.toUpperCase();
+  }
+  return message.toLowerCase();
+}
+console.log(registerUser("upper")); // "HELLO WORLD"
+
+// ***********************************************
+
+const stringText = "script.min.js";
+console.log(stringText.slice(0, 10));
+
+// ***********************************************
+
+function leightText(text, maxLength) {
+  maxLength = 10;
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + "...";
+  }
+  return text;
+}
+
+console.log(leightText("Hello my friend"));
+
+// ***********************************************
+
+function short(text, maxLength) {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + "...";
+  }
+  return text;
+}
+
+console.log(short("JavaScript", 4));
+console.log(short("Cat", 10));
+console.log(short("Hello world", 5));
+
+// ***********************************************
+
+function isSpam(message) {
+  const text = message.toLowerCase();
+  if (
+    text.includes("buy") ||
+    text.includes("sale") ||
+    text.includes("discount")
+  ) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isSpam("BUY NOW!"));
+console.log(isSpam("Big SALE today"));
+console.log(isSpam("Huge DISCOUNT!!!"));
+console.log(isSpam("Hello my friend"));
+console.log(isSpam("bUy SaLe"));
+
+// ***********************************************
+
+function extractExtension(fileName) {}
